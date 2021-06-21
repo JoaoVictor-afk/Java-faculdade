4b9a6e215edf681286bbbf71ccb7ae2b783f32c0
João Victor Peixoto de Souza <82658732+JoaoVictor-afk@users.noreply.github.com>
2021-06-21 17:03:29 -0300

    Update circulodnv.java

--- Exercicios/circulodnv.java
+++ Exercicios/circulodnv.java
@@ -1,1 +1,40 @@
+class circulodnv {
+    public   double raio;
+    public   double area;
+    
+    public circulodnv(double r, double a) {
+    
+        this.raio=1;
+        this.area=a;
+    
+        
+    }
+    
+    public circulodnv() {     
+    }
+    
+    
+    public  double getRaio(){
+        return raio;
+    }
+    
+    public  double getArea(){
+        return area=(Math.pow(raio, 2)*3.14);
+    }
+    
+
+    public static void main(String args [] ){
+        
+        circulodnv areas = new circulodnv();
+        
+        areas.raio=6;
+        
+        System.out.println(areas.raio);
+        System.out.println(areas.getArea());
+        
+    }
+    
+    
+    
+}
 
